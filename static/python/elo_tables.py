55715d959ed0431cf5b79acbf173f0e0499e7d0f
Syver <syvjohansen@yahoo.com>
2024-07-20 17:38:52 -0400

    Lots of changes made to freeze rows and adding pages for each skier.  Next up is to add pictures for each skier, results for each race with Elo, then it's elo percentage, then it's a page for all time. Finally it's to add tableau

--- static/python/elo_tables.py
+++ static/python/elo_tables.py
@@ -11,6 +11,7 @@ L_Sprint_C = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/exc
 L_Sprint_F = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/L_Sprint_F.feather')
 L_C = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/L_C.feather')
 L_F = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/L_F.feather')
+L_chrono = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/ladies_chrono.feather')
 
 M = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/M.feather')
 M_Distance = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/M_Distance.feather')
@@ -21,6 +22,7 @@ M_Sprint_C = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/exc
 M_Sprint_F = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/M_Sprint_F.feather')
 M_C = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/M_C.feather')
 M_F = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/M_F.feather')
+M_chrono = pd.read_feather('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/men_chrono.feather')
 
 
 # List of DataFrames
@@ -31,6 +33,29 @@ dfs = [L, L_Distance, L_Distance_C, L_Distance_F, L_Sprint, L_Sprint_C, L_Sprint
 dfs_str = ["L", "L_Distance", "L_Distance_C", "L_Distance_F", "L_Sprint", "L_Sprint_C", "L_Sprint_F",
        "L_C", "L_F", "M", "M_Distance", "M_Distance_F", "M_Distance_C", "M_Sprint", "M_Sprint_C", "M_Sprint_F",
        "M_C", "M_F"]
+
+chronos = [L_chrono, M_chrono]
+chronos_str = ["L_chrono", "M_chrono"]
+
+
+
+def process_chrono(df, file_path):
+    # Filter the DataFrame to keep only the rows with the maximum date
+    df_current = df.loc[df['Date'] == max(df['Date'])]
+    
+    # Sort the filtered DataFrame by the "Elo" column in descending order
+    df_current = df_current.sort_values(by="Elo", ascending=False)
+    
+    # Add a "Place" column with values ranging from 1 to the length of the DataFrame
+    df_current['Place'] = range(1, len(df_current) + 1)
+    
+    # Select the desired columns
+    df_current = df_current[["Place", "Skier", "Nation", "Age", "Pelo","Distance_Pelo", "Distance_C_Pelo", "Distance_F_Pelo",
+    "Sprint_Pelo", "Sprint_C_Pelo", "Sprint_F_Pelo", "Classic_Pelo", "Freestyle_Pelo", "ID"]]
+    print(df_current)
+    
+    # Save the result to a JSON file
+    df_current.to_json(file_path, orient='records', lines=False)
 
 # Function to process each DataFrame
 def process_df(df, file_path):
@@ -44,16 +69,19 @@ def process_df(df, file_path):
     df_current['Place'] = range(1, len(df_current) + 1)
     
     # Select the desired columns
-    df_current = df_current[["Place", "Skier", "Nation", "Age", "Pelo"]]
+    df_current = df_current[["Place", "Skier", "Nation", "Age", "Pelo" , "ID"]]
+    print(df_current)
     
     # Save the result to a JSON file
     df_current.to_json(file_path, orient='records', lines=False)
 
 # Process each DataFrame and save the results to JSON files
-for i, df in enumerate(dfs):
+#for i, df in enumerate(dfs):
+for i, df in enumerate(chronos):	
     print(i)
-    file_path = f"/Users/syverjohansen/blog/daehl-e/static/python/excel365/{dfs_str[i]}_current.json"
-    process_df(df, file_path)
+    file_path = f"/Users/syverjohansen/blog/daehl-e/static/python/excel365/{chronos_str[i]}_current.json"
+    #process_df(df, file_path)
+    process_chrono(df, file_path)
 
 
 
@@ -78,7 +106,7 @@ M_C.to_csv('/Users/syverjohansen/blog/daehl-e/static/python/excel365/M_C.csv')
 M_F.to_csv('/Users/syverjohansen/blog/daehl-e/static/python/excel365/M_F.csv')'''
 
 
-L.to_json("/Users/syverjohansen/blog/daehl-e/static/python/excel365/L.json", 
+'''L.to_json("/Users/syverjohansen/blog/daehl-e/static/python/excel365/L.json", 
 	orient = 'records', lines=False)
 L_Distance.to_json("/Users/syverjohansen/blog/daehl-e/static/python/excel365/L_Distance.json", 
 	orient = 'records', lines=False)
@@ -118,7 +146,7 @@ M_Sprint_F.to_json("/Users/syverjohansen/blog/daehl-e/static/python/excel365/M_S
 M_C.to_json("/Users/syverjohansen/blog/daehl-e/static/python/excel365/M_C.json",
 	orient = 'records', lines=False)
 M_F.to_json("/Users/syverjohansen/blog/daehl-e/static/python/excel365/M_F.json",
-	orient = 'records', lines=False)
+	orient = 'records', lines=False)'''
 
 
 
---
+++ static/python/skier_tables.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import polars as pl
+import json
+import os
+
+
+L_chrono = pl.read_ipc('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/ladies_chrono.feather')
+
+
+M_chrono = pl.read_ipc('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/men_chrono.feather')
+
+L_chrono = L_chrono.filter(pl.col("City")!="Summer")
+M_chrono = M_chrono.filter(pl.col("City")!="Summer")
+output_dir_L = '/Users/syverjohansen/blog/daehl-e/content/skiers/L'
+output_dir_M = '/Users/syverjohansen/blog/daehl-e/content/skiers/M'
+
+unique_ladies = L_chrono.select("ID").unique()
+unique_men = M_chrono.select("ID").unique()
+
+
+def process_chrono(df, ID, sex):
+    # Filter the DataFrame to keep only the rows with the maximum date
+    df= df.filter(pl.col("ID")==ID)
+    
+    # Sort the filtered DataFrame by the "Elo" column in descending order
+    df = df.sort(["Season", "Race"])
+
+    
+    # Select the desired columns
+    df = df.select(["Exp", "Date", "City", "Country", "Event", "Distance", "MS", "Technique", "Place", "Season", "Race", "Age", "Elo","Distance_Elo", "Distance_C_Pelo", "Distance_F_Pelo",
+    "Sprint_Elo", "Sprint_C_Elo", "Sprint_F_Elo", "Classic_Elo", "Freestyle_Elo", "ID"])
+
+    file_path = f"/Users/syverjohansen/blog/daehl-e/static/python/excel365/{sex}/{ID}.json"
+    # Save the result to a JSON file
+    json_str = df.write_json(file_path)
+    #with open(file_path, 'w') as f:
+    #    f.write(json_str)
+
+for id_value in unique_ladies["ID"]:
+    process_chrono(L_chrono, id_value, "L")
+
+
+for id_value in unique_men["ID"]:
+    process_chrono(M_chrono, id_value, "M")
+
+for row in unique_ladies.iter_rows(named=True):
+    skier_id = row['ID']
+    skier  = L_chrono.filter(pl.col("ID")==skier_id)[-1, "Skier"]
+    
+    # Create Markdown content for each skier
+    markdown_content = f"""
+---
+title: Skier Profile for {skier}
+---
+
+# Skier Profile
+
+This page is for: {skier}.
+    """
+
+    # Save the Markdown file
+    with open(os.path.join(output_dir_L, f'{skier_id}.md'), 'w') as file:
+        file.write(markdown_content)
+
+    print(f"Generated Markdown for skier ID {skier}")
+
+
+
+
+print("All files have been processed.")
+
+
+for row in unique_men.iter_rows(named=True):
+    skier_id = row['ID']
+    skier  = M_chrono.filter(pl.col("ID")==skier_id)[-1, "Skier"]
+    
+    # Create Markdown content for each skier
+    markdown_content = f"""
+---
+title: Skier Profile for {skier}
+---
+
+# Skier Profile
+
+This page is for: {skier}.
+    """
+
+    # Save the Markdown file
+    with open(os.path.join(output_dir_M, f'{skier_id}.md'), 'w') as file:
+        file.write(markdown_content)
+
+    print(f"Generated Markdown for skier ID {skier}")
+
+
+
+
+print("All files have been processed.")
+
+#Convert to excel files for each in /Users/syverjohansen/blog/daehl-e/static/python/excel365
+
+
