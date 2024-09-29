975b611c7eba8748d9f041f63b421ac278a0bc0b
Syver <syvjohansen@yahoo.com>
2024-09-29 18:02:32 -0400

    Committing to reduce file size.  Couldn't figure out query parameters.  That will have to be a later venture

--- public/python/skier_tables.py
+++ public/python/skier_tables.py
@@ -10,20 +10,33 @@ L_chrono = pl.read_ipc('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/
 
 M_chrono = pl.read_ipc('/Users/syverjohansen/ski/elo/python/ski/polars/excel365/men_chrono.feather')
 
-L_chrono = L_chrono.filter(pl.col("City")!="Summer")
-M_chrono = M_chrono.filter(pl.col("City")!="Summer")
+#L_chrono = L_chrono.filter(pl.col("City")!="Summer")
+#M_chrono = M_chrono.filter(pl.col("City")!="Summer")
 
 L_chrono = L_chrono.with_columns(
-    pl.col("Date").str.strptime(pl.Date, "%Y-%m-%d")
+    pl.col("Date").str.strptime(pl.Date, "%Y-%m-%d", strict=False)
 )
 
 
 M_chrono = M_chrono.with_columns(
-    pl.col("Date").str.strptime(pl.Date, "%Y-%m-%d")
+    pl.col("Date").str.strptime(pl.Date, "%Y-%m-%d", strict=False)
 )
 
 
-
+def get_country_code(nation):
+    codes = {
+"Algeria":"alg", "Andorra":"and", "Argentina":"arg", "Armenia":"arm", "Australia":"aus", "Austria":"aut", "Belarus":"blr", "Belgium":"bel", "Bermuda":"ber", "Bolivia":"bol", "Bosnia&Herzegovina":"bih", "Brazil":"bra", "Bulgaria":"bul",
+"Cameroon":"cmr", "Canada":"can", "Chile":"chi", "China":"chn", "Colombia":"col", "Costa Rica":"crc", "Croatia":"cro", "Czechia":"cze", "Denmark":"den",
+"Ecuador":"ecu", "Estonia":"est", "Ethiopia":"eth", "Fiji":"fij", "Finland":"fin", "France":"fra",
+"Germany":"ger", "Great Britain":"gbr", "Greece":"gre", "Guatemala":"gua", "Haiti":"hai", "Honduras":"hon", "Hungary":"hun", 
+"Iceland":"isl", "India":"ind", "Iran":"iri", "Ireland":"irl", "Israel":"isr", "Italy":"ita", "Japan":"jpn", 
+"Kazakhstan":"kaz", "Kenya":"ken", "Kyrgyzstan":"kgz", "Latvia":"lat", "Lebanon":"lbn", "Liechtenstein":"lie", "Lithuania":"ltu", "Luxembourg":"lux",
+"Mexico":"mex", "Moldova":"mda", "Mongolia":"mgl", "Montenegro":"mne", "Morocco":"mar", "Nepal":"nep", "Netherlands":"ned", "New Zealand":"nzl", "Nigeria":"ngr", "North Korea":"prk", "North Macedonia":"mkd", "Norway":"nor",
+"Pakistan":"pak", "Peru":"per", "Poland":"pol", "Portugal":"por", "Romania":"rou", "Russia":"rus",
+"San Marino":"smr", "Serbia":"srb", "Slovakia":"svk", "Slovenia":"slo", "South Africa":"rsa", "South Korea":"kor", "Soviet":"urs", "Spain":"esp", "Sweden":"swe", "Switzerland":"sui", "Taiwan":"tpe", "Thailand":"tha", "Togo":"tog", "Tonga":"tga", "Trinidad & Tobago":"tto", "Turkey":"tur",
+"USA":"usa", "Ukraine":"ukr", "Venezuela":"ven"
+    }
+    return codes[nation]
 
 
 def get_season(df, season, min_season):
@@ -123,10 +136,12 @@ def process_season(df, ID, sex):
     "Sprint_Elo_Pct", "Sprint_C_Elo_Pct", "Sprint_F_Elo_Pct", "Classic_Elo_Pct", "Freestyle_Elo_Pct", "ID"])
 
     file_path = f"/Users/syverjohansen/blog/daehl-e/static/python/excel365/{sex}/{ID}.json"
+    
     # Save the result to a JSON file
     json_str = df.write_json(file_path)
 
-
+output_dir_L = '/Users/syverjohansen/blog/daehl-e/content/skiers/l'
+output_dir_M = '/Users/syverjohansen/blog/daehl-e/content/skiers/m'
 ladies_seasons = L_chrono.select("Season").unique()
 ladies_seasons = ladies_seasons.sort("Season", descending=False)
 min_ladies = min(ladies_seasons["Season"])
@@ -149,6 +164,62 @@ for id_value in unique_ladies["ID"]:
 
 for id_value in unique_men["ID"]:
     process_season(men_season_df, id_value, "m")
+
+for row in unique_ladies.iter_rows(named=True):
+    skier_id = row['ID']
+    skier  = L_chrono.filter(pl.col("ID")==skier_id)[-1, "Skier"]
+    nation = L_chrono.filter(pl.col("ID")==skier_id)[-1, "Nation"]
+    nation = get_country_code(nation)
+    # Create Markdown content for each skier
+    markdown_content = f"""---
+title: Skier Profile for {skier}
+sex: "L"
+id: "{skier_id}"
+image: "/img/flags/{nation}.svg" 
+---
+
+# Skier Profile
+
+This page is for: {skier}.
+    """
+
+    # Save the Markdown file
+    with open(os.path.join(output_dir_L, f'{skier_id}.md'), 'w') as file:
+        file.write(markdown_content)
+
+    print(f"Generated Markdown for skier ID {skier}")
+
+
+
+
+print("All files have been processed.")
+
+
+for row in unique_men.iter_rows(named=True):
+    skier_id = row['ID']
+    skier  = M_chrono.filter(pl.col("ID")==skier_id)[-1, "Skier"]
+    nation = M_chrono.filter(pl.col("ID")==skier_id)[-1, "Nation"]
+    nation = get_country_code(nation)
+    # Create Markdown content for each skier
+    markdown_content = f"""---
+title: Skier Profile for {skier}
+sex: "M"
+id: "{skier_id}"
+image: "/img/flags/{nation}.svg" 
+---
+
+# Skier Profile
+
+This page is for: {skier}.
+    """
+
+    # Save the Markdown file
+    with open(os.path.join(output_dir_M, f'{skier_id}.md'), 'w') as file:
+        file.write(markdown_content)
+
+    print(f"Generated Markdown for skier ID {skier}")
+
+
 
 
 
